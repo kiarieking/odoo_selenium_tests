8dc47f956da744a48de676d73695c1dbb118c912
kkiarie <kiariekevin22@gmail.com>
2025-09-09 12:29:11 +0300

    [ADD] logout and invalid login methods

--- authentication/test_login.py
+++ authentication/test_login.py
@@ -11,14 +11,26 @@ def login(driver):
         driver.find_element(By.ID, "login").send_keys(email)
         driver.find_element(By.ID, "password").send_keys(password)
         driver.find_element(By.XPATH, "//button[@type='submit' and contains(@class, 'btn-primary')]").click()
-        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//a[@class='o_menu_brand' and normalize-space()='Discuss']")))
-
-        assert "Discuss" in driver.page_source
     return _login
+
+
+def logout(driver):
+    driver.get('https://sandbox.erp.quatrixglobal.com/')
+    usermenu = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.oe_topbar_name")))
+    usermenu.click()
+    driver.find_element(By.XPATH, "//a[@data-menu='logout']").click()
+
+    
     
 def test_valid_login(driver, login):
     login("kelvin.kiarie@quatrixglobal.com", "$kingara120")
+    WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//a[@class='o_menu_brand' and normalize-space()='Discuss']")))
     assert "Discuss" in driver.page_source
-
-def test_invalid_login(driver, login):
+    logout(driver)
     
+def test_invalid_login(driver, login):
+    login("test123432@email.com", "pwd123")
+    WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.alert.alert-danger[role='alert']")))
+    assert "Wrong login/password" in driver.page_source 
+    
+   
