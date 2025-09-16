acee6da89baf052bd4c8e958aed7d109d9c328f4
kiarieking <kiariekevin22@gmail.com>
2025-09-16 21:42:37 +0300

    [ADD] Test to open dispatch and .env files for the base64 image icons

--- dispatch_module/test_create_dispatch.py
+++ dispatch_module/test_create_dispatch.py
@@ -1,2 +1,12 @@
-def test_create_dispatch(driver,login):
-    login()
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+EMAIL = os.getenv('EMAIL')
+PASSWORD = os.getenv('PASSWORD')
+
+def test_create_dispatch(driver,login,dispatch_icon):
+    login(EMAIL,PASSWORD)
+    dispatch_icon()
+    
