3d1e2e1a68b8b6b515976df3d27945f5105c812b
kkiarie <kiariekevin22@gmail.com>
2025-09-17 10:54:23 +0300

    [ADD] Add shipper, vehicle and product methods for create dispatch

--- dispatch_module/test_create_dispatch.py
+++ dispatch_module/test_create_dispatch.py
@@ -13,11 +13,37 @@ def test_create_dispatch(driver,login,dispatch_icon):
     login(EMAIL,PASSWORD)
     dispatch_icon()
     open_new_dispatch(driver)
+    add_shipper(driver)
+    add_vehicle(driver)
     
 def open_new_dispatch(driver):
     create_btn = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//button[.//span[normalize-space(text())='Create']]")))
     create_btn.click()
     time.sleep(3)
+
+def add_shipper(driver):
+    shipper_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME,"partner_id")))
+    shipper_input.click()
+    new_shipper = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "KEDA CERAMICS INTERNATIONAL COMPANY LIMITED")))
+    new_shipper.click()
+    time.sleep(5)
+
+def add_vehicle(driver):
+    vehicle_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "vehicle_id")))
+    vehicle_input.click()
+    new_vehicle = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, "Mitsubishi/Mitsubishi/ExtraVehicle#1")))
+    new_vehicle.click()
+    time.sleep(5)
+
+def add_product_line(driver):
+    add_line = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, "Add a line")))
+    add_line.click()
+    product_input = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.NAME, "product_id")))
+    product_input.click()
+    product = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, "[KEDA] CHAVAKALI 28T")))
+    product.click()
+
+
 
 
 
