ffd4d605a5d472a737a5da1616d90d473a4bc015
kkiarie <kiariekevin22@gmail.com>
2025-09-17 16:10:23 +0300

    [ADD] started edit vehicle tests

--- dispatch_module/test_edit_dispatch.py
+++ dispatch_module/test_edit_dispatch.py
@@ -12,13 +12,17 @@ PASSWORD = os.getenv('PASSWORD')
 def test_create_dispatch(driver,login,dispatch_icon):
     login(EMAIL,PASSWORD)
     dispatch_icon()
+    status = "Quotation"
+    dispatch_no = "DO10613"
+    group_dispatch(driver)
+    open_dispatch(driver,status,dispatch_no)
 
 def group_dispatch(driver):
     group_by = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//i[@class='fa fa-bars']/following-sibling::span[text()='Group By']")))
     group_by.click()
     status = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//a[@aria-checked='false' and @role='menuitemcheckbox' and text()='Status']")))
     status.click()
-    time.sleep(3)
+    # time.sleep(3)
 
 
 def open_dispatch(driver,status,dispatch_no):
@@ -28,4 +32,26 @@ def open_dispatch(driver,status,dispatch_no):
     dispatch_xpath = f"(.//*[normalize-space(text()) and normalize-space(.)='{dispatch_no}'])[1]/following::td[1]"
     dispatch = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, dispatch_xpath)))
     dispatch.click()
+    # time.sleep(3)
+
+def start_editing_dispatch(driver):
+    edit_btn = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Edit']")))
+    edit_btn.click()
+
+def add_shipper(driver):
+    shipper_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME,"partner_id")))
+    shipper_input.click()
+    new_shipper = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "KEDA CERAMICS INTERNATIONAL COMPANY LIMITED")))
+    new_shipper.click()
+    time.sleep(5)
+
+def add_vehicle(driver):
+    vehicle_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "vehicle_id")))
+    vehicle_input.click()
+    new_vehicle = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, "Mitsubishi/Mitsubishi/ExtraVehicle#1")))
+    new_vehicle.click()
+    time.sleep(5)
+
+
+
 
