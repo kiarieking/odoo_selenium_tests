1edf882d41b1e79f880e643d6a0b2c0d26cafc60
kkiarie <kiariekevin22@gmail.com>
2025-09-22 11:45:41 +0300

    [ADD] Started edit voucher tests

--- fuelvoucher_module/test_edit_voucher.py
+++ fuelvoucher_module/test_edit_voucher.py
@@ -1,3 +1,23 @@
+from dotenv import load_dotenv
+import os
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+def test_create_voucher(driver,login,fuel_icon):
+    login(EMAIL,PASSWORD)
+    fuel_icon()
+    group_vouchers(driver)
+    status = "Quotation"
+    voucher_no = "FO3930"
+    open_voucher(driver,status,voucher_no)
+
+
 def group_vouchers(driver):
     group_by = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//i[@class='fa fa-bars']/following-sibling::span[text()='Group By']")))
     group_by.click()
@@ -11,4 +31,5 @@ def open_voucher(driver,status,voucher_no):
     voucher_xpath = f"(.//*[normalize-space(text()) and normalize-space(.)='{voucher_no}'])[1]/following::td[1]"
     voucher = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, voucher_xpath)))
     voucher.click()
+    time.sleep(3)
     
