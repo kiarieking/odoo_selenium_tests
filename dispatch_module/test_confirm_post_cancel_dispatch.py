5c2e63802b6805f620a60688eec12dfd68df3a60
kkiarie <kiariekevin22@gmail.com>
2025-10-29 11:33:02 +0300

    [REFACTOR] Changed dispatch tests for post and cancel to include their own login in methods

--- dispatch_module/test_confirm_post_cancel_dispatch.py
+++ dispatch_module/test_confirm_post_cancel_dispatch.py
@@ -21,25 +21,25 @@ def test_confirm_dispatch(driver,login,dispatch_icon):
     complete_delivery(driver)
     time.sleep(3)
 
-# @pytest.mark.order(16)
-# def test_post_dispatch(driver,login,dispatch_icon):
-#     status = "Dispatch Order"
-#     dispatch_no = "DO9028"
-#     # login(EMAIL,PASSWORD)
-#     dispatch_icon()
-#     group_dispatch(driver)
-#     open_dispatch(driver,status,dispatch_no)
-#     post_dispatch(driver)
+@pytest.mark.order(16)
+def test_post_dispatch(driver,login,dispatch_icon):
+    status = "Dispatch Order"
+    dispatch_no = "DO9028"
+    login(EMAIL,PASSWORD)
+    dispatch_icon()
+    group_dispatch(driver)
+    open_dispatch(driver,status,dispatch_no)
+    post_dispatch(driver)
 
-# @pytest.mark.order(17)
-# def test_cancel_dispatch(driver, login, dispatch_icon):
-#     status = "Posted"
-#     dispatch_no = "DO9991"
-#     # login(EMAIL,PASSWORD)
-#     dispatch_icon()
-#     group_dispatch(driver)
-#     open_dispatch(driver,status,dispatch_no)
-#     cancel_dispatch(driver)
+@pytest.mark.order(17)
+def test_cancel_dispatch(driver, login, dispatch_icon):
+    status = "Posted"
+    dispatch_no = "DO9991"
+    login(EMAIL,PASSWORD)
+    dispatch_icon()
+    group_dispatch(driver)
+    open_dispatch(driver,status,dispatch_no)
+    cancel_dispatch(driver)
 
 
 def group_dispatch(driver):
@@ -61,6 +61,7 @@ def open_dispatch(driver,status,dispatch_no):
 def complete_delivery(driver):
     edit_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='Edit']")))
     edit_btn.click()
+    time.sleep(5)
     pod_input = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//input[@type='file' and @name='ufile']")))
     pod_input.send_keys("/home/kkiarie/Downloads/sample.pdf")
     time.sleep(5)
