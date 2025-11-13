172bb4f00ef431796024a8cb90f01a7c0e9f93c3
kkiarie <kiariekevin22@gmail.com>
2025-11-13 12:52:16 +0300

    [REFACTOR] Changed search more and select customer locators on edit billing test

--- billing_module/test_edit_billing.py
+++ billing_module/test_edit_billing.py
@@ -20,7 +20,7 @@ def test_edit_billing(driver,login,billing_icon):
     time.sleep(2)
 
 def open_bill(driver,bill_no):
-    invoiced_bills = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//a[@class='o_menu_entry_lvl_1']//span[normalize-space()='Invoiced Bills']")))
+    invoiced_bills = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//a[@data-menu-xmlid='quatrix_billing_module.orders_menu']")))
     invoiced_bills.click()
     bill_xpath = f"(.//*[normalize-space(text()) and normalize-space(.)='{bill_no}'])[1]/following::td[1]"
     bill = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, bill_xpath)))
@@ -31,13 +31,13 @@ def edit_bill(driver):
      edit_btn.click()
      vehicle_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "vehicle_id")))
      vehicle_input.click()
-     search_more_vehicle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[contains(@class, 'o_m2o_dropdown_option')]/a[@class='ui-menu-item-wrapper' and text()='Search More...']")))        
+     search_more_vehicle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='dropdown-item ui-menu-item-wrapper' and normalize-space(text())='Search More...']")))        
      search_more_vehicle.click()
-     select_vehicle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//td[@class='o_data_cell o_field_cell o_list_char' and @title='KAA703C' and text()='KAA703C']")))
+     select_vehicle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//td[@name='license_plate' and normalize-space(text())='KAA744A']")))
      select_vehicle.click()
      customer_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "client_id")))
      customer_input.click()
-     select_customer = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//li[@class='ui-menu-item']//a[@class='ui-menu-item-wrapper' and text()='ABDULAHI WALAIKA']")))
+     select_customer = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//a[@class='dropdown-item ui-menu-item-wrapper' and normalize-space(text())='ABDULAHI WALAIKA']")))
      select_customer.click()
      reference = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "reference_number")))
      reference.clear()
