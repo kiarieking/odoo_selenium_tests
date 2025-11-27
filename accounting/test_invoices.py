986d7e3a8917cd11f67878200c09655c35c2d571
kkiarie <kiariekevin22@gmail.com>
2025-11-27 11:13:49 +0300

    [ADD] Finished on edit details method for edit invoice

--- accounting/test_invoices.py
+++ accounting/test_invoices.py
@@ -32,6 +32,17 @@ def test_preview_invoice(driver,login,accounting_icon):
     preview_invoice(driver)
     time.sleep(3)
 
+@pytest.mark.order(20)
+def test_cancel_invoice(driver,login,accounting_icon):
+    login(EMAIL,PASSWORD)
+    accounting_icon()
+    group_invoices(driver)
+    status = "Draft"
+    invoice_no = "INV/2023/0844"
+    open_invoices(driver,status,invoice_no)
+    cancel_invoice(driver)
+    time.sleep(3)
+
 
 def group_invoices(driver):
     customers_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[normalize-space()='Customers']]")))
@@ -75,3 +86,12 @@ def preview_invoice(driver):
         "//a[contains(@class,'o_download_btn')] | //a[contains(@class,'o_portal_invoice_print')] | //div[contains(.,'This is a preview of the customer portal')]"
     ))
     )
+
+def cancel_invoice(driver):
+    cancel_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[@name='button_cancel' and .//span[normalize-space()='Cancel']]")))
+    cancel_btn.click()
+    time.sleep(2)
+    status = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class,'o_arrow_button') and @data-value='cancel']")))
+    title = status.get_attribute("title")
+    assert title == "Current state"
+    
