34b7300b4bc6176526be8bb93a6bea2e7490daa6
kkiarie <kiariekevin22@gmail.com>
2025-11-27 13:27:36 +0300

    [ADD] invoice line locator

--- accounting/test_edit_invoice.py
+++ accounting/test_edit_invoice.py
@@ -18,7 +18,8 @@ def test_edit_invoice(driver,login,accounting_icon):
     status = "Draft"
     invoice_no = "INV/2023/0844"
     open_invoices(driver,status,invoice_no)
-    edit_invoice_details(driver) 
+    # edit_invoice_details(driver) 
+    edit_invoice_line(driver)
     time.sleep(3)
 
 def group_invoices(driver):
@@ -56,5 +57,19 @@ def edit_invoice_details(driver):
     customer.click()
     select_customer = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[contains(@class,'ui-menu-item-wrapper') and normalize-space()='IN MOTION REGIONAL LOGISTICS LIMITED']")))
     select_customer.click()
-    due_date = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//input[contains(@class,'o_input') and contains(@class,'ui-autocomplete-input') and @placeholder='Terms']")))
-    due_date.click()
+    payment_terms = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//input[@placeholder='T﻿e﻿r﻿m﻿s' and contains(@class,'ui-autocomplete-input')]")))
+    payment_terms.click()
+    select_terms = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[normalize-space()='30 Days']")))
+    select_terms.click()
+    payment_reference = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//input[contains(@class,'o_field_char') and @name='payment_reference']")))
+    payment_reference.click()
+    payment_reference.clear()
+    payment_reference.send_keys("INV/2023/0844")
+    save_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//span[normalize-space()='Save']")))
+    save_btn.click()
+
+def edit_invoice_line(driver):
+    edit_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(@class,'o_form_button_edit') and @title='Edit record']")))
+    edit_btn.click()
+    line_row = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//tr[.//span[contains(@class,'o_tag_badge_text') and text()='VAT']]")))
+    line_row.click()
