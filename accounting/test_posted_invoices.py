773b6f013181b2d63efb850afa474f460679254e
kiarieking <kiariekevin22@gmail.com>
2025-12-01 19:25:55 +0300

    [ADD/REFACTOR] Added posted invoice test file and rename test_invoice to test_drafted_invoice

---
+++ accounting/test_posted_invoices.py
@@ -0,0 +1,11 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import pytest
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
