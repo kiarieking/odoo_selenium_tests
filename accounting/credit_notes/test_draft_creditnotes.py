d0e68de38db71de468af1059e29df0a281bc3be9
kiarieking <kiariekevin22@gmail.com>
2025-12-02 12:54:17 +0300

    [ADD] Started on credit note tests

---
+++ accounting/credit_notes/test_draft_creditnotes.py
@@ -0,0 +1,45 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import pytest
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+@pytest.mark.order(25)
+def test_confirm_creditnote(driver,login,accounting_icon):
+    login(EMAIL,PASSWORD)
+    accounting_icon()
+    group_creditnote(driver)
+    status = "Draft"
+    invoice_no = "RINV/2025/0014"
+    open_creditnote(driver,status,invoice_no)
+
+def group_creditnote(driver):
+    customers_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[normalize-space()='Customers']]")))
+    customers_btn.click()
+    invoices = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[normalize-space()='Credit Notes']")))
+    invoices.click()
+    WebDriverWait(driver,15).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//li[contains(@class,'breadcrumb-item') and contains(@class,'active')]//span[normalize-space()='Credit Notes']")
+        )
+    )
+
+    group_by = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//span[@class='o_dropdown_title' and normalize-space()='Group By']")))
+    group_by.click()
+    status = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//span[@role='menuitemcheckbox' and normalize-space()='Status']")))
+    status.click()
+
+def open_creditnote(driver,status,invoice_no):
+    status_xpath = f"//th[@class='o_group_name' and contains(normalize-space(), '{status}')]"
+    invoice_grp = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,status_xpath)))
+    invoice_grp.click()
+    invoice_xpath = "//td[contains(@class,'o_data_cell') and @title='RINV/2025/0016']"
+
+    invoice = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,invoice_xpath)))
+    invoice.click()
