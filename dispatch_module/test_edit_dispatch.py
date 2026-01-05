c81758c1c4d07f215fe3b4d76aa1390c4e9ccf82
kkiarie <kiariekevin22@gmail.com>
2026-01-05 11:51:29 +0300

    [REFACTOR] refactored the opening of dispatch to open the first row instead of relying on a dispatch no.

--- dispatch_module/test_edit_dispatch.py
+++ dispatch_module/test_edit_dispatch.py
@@ -17,8 +17,8 @@ def test_edit_dispatch(driver,login,dispatch_icon):
     time.sleep(3)
     dispatch_icon()
     status = "Quotation"
-    dispatch_no = "DO11192"
     group_dispatch(driver)
+    dispatch_no = get_dispatch_no(driver)
     open_dispatch(driver,status,dispatch_no)
     start_editing_dispatch(driver)
     edit_shipper(driver)
@@ -26,7 +26,16 @@ def test_edit_dispatch(driver,login,dispatch_icon):
     edit_product_line(driver)
 
 
+def get_dispatch_no(driver):
+    dispatch = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"(//tr[contains(@class,'o_data_row') and not(contains(@style,'display: none'))])[1]//td[@name='name']")))
+    dispatch_no = dispatch.text.strip()
+    print (dispatch_no)
+    return dispatch_no
+
 def group_dispatch(driver):
+    table = driver.find_element(By.XPATH, "//table")
+    # apply filter here
+    WebDriverWait(driver, 10).until(EC.staleness_of(table))
     group_by = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='dropdown-toggle btn btn-light ' and .//span[normalize-space()='Group By']]")))
     group_by.click()
     status = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//span[@role='menuitemcheckbox' and normalize-space()='Status']")))
@@ -34,11 +43,11 @@ def group_dispatch(driver):
     # time.sleep(3)
 
 
-def open_dispatch(driver,status,dispatch_no):
+def open_dispatch(driver,status):
     status_xpath = f"//th[@class='o_group_name' and contains(., '{status}')]"
     status = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, status_xpath)))
     status.click()
-    dispatch_xpath = f"(.//*[normalize-space(text()) and normalize-space(.)='{dispatch_no}'])[1]/following::td[1]"
+    dispatch_xpath = "//tbody/tr[contains(@class,'o_data_row')][1]"
     dispatch = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, dispatch_xpath)))
     dispatch.click()
     # time.sleep(3)
