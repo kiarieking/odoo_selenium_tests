27ac2a9c41d4e0e739794ce0d0b9d24fb825915a
kiarieking <kiariekevin22@gmail.com>
2026-02-21 10:46:55 +0300

    [ADD] Added group_by method in Group_Open_doc class

--- accounting/invoices/Group_Open_doc.py
+++ accounting/invoices/Group_Open_doc.py
@@ -1,7 +1,22 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 class Group_Open_doc:
-    def __init__(self,status,group_by):
-        self.status = status
-        self.group_by = group_by
+    def __init__(self):
+        pass
 
-    def group_by(self):
-        
+    def group_by(self,driver):
+        customers_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[normalize-space()='Customers']]")))
+        customers_btn.click()
+        invoices = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[normalize-space()='Invoices']")))
+        invoices.click()
+        WebDriverWait(driver,15).until(
+            EC.visibility_of_element_located(
+                (By.XPATH, "//li[contains(@class,'breadcrumb-item') and contains(@class,'active')]//span[normalize-space()='Invoices']")
+            )
+        )
+
+        group_by_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//span[@class='o_dropdown_title' and normalize-space()='Group By']")))
+        group_by_btn.click()
+        status = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//span[@role='menuitemcheckbox' and normalize-space()='Status']")))
+        status.click()
